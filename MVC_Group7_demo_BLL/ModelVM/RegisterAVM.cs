b7a9f64eabb40ff03c878a5b6a0f5544efdb88fb
Moaz715 <moazfathy592005@gmail.com>
2025-08-14 23:05:37 +0300

    Create RegisterAVM.cs

---
+++ MVC_Group7_demo_BLL/ModelVM/RegisterAVM.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Group7_demo_BLL.ModelVM
+{
+    public class RegisterAVM
+    {
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
