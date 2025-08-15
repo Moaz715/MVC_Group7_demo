3f4b148f0d208d24fba6882b1aa9568abde8b5b0
Moaz715 <moazfathy592005@gmail.com>
2025-08-15 15:33:55 +0300

    Update RegisterVM.cs

--- MVC_Group7_demo_BLL/ModelVM/RegisterVM.cs
+++ MVC_Group7_demo_BLL/ModelVM/RegisterVM.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,17 +9,21 @@ namespace MVC_Group7_demo_BLL.ModelVM
 {
     public class RegisterVM
     {
-        [Required]
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
-        [Required]
-        public string Username { get; set; }
-        [Required]
-        public string Phone { get;  set; }
-        [Required]
-        public string Email { get;  set; }
-        [Required]
-        public string Password { get;  set; }
-        [Required]
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [MinLength(11, ErrorMessage = "Phone number must be 11 digits")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
         public string Location { get; set; }
     }
 }
